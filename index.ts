98e77c9b2d09fbf076ad1fca7e3d2377742a49e4
roysohom88 <roy.sohom88@yahoo.in>
2021-06-22 12:31:45 +0530

    delay rxjs

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 // Import RxJS;
-import { of, Observable, interval } from 'rxjs';
-import { map, filter, take, takeWhile } from 'rxjs/operators';
+import { of, Observable, interval, fromEvent } from 'rxjs';
+import { map, filter, take, takeWhile, delay } from 'rxjs/operators';
 interval(1000)
   .pipe(
+    takeWhile(() => true),
     take(3),
     map(v => Date.now())
   )
   .subscribe(value => console.log('Subscriber: ' + value));
 //emit 1,2,3,4,5
 //const source = of(1, 2, 3, 4, 5);
-//allow values until value from source is greater than 4, then complete
-//const example = source.pipe(takeWhile(val => val <= 4));
-//output: 1,2,3,4
-//const subscribe = example.subscribe(val => console.log(val));
+
+const clicks = fromEvent(document, 'click');
+const delayedClicks = clicks.pipe(delay(1000)); // each click emitted after 1 second
+delayedClicks.subscribe(x => console.log(x));
