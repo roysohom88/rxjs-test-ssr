c40831a3e04305232d86a309cde2f6ae82a17c87
roysohom88 <roy.sohom88@yahoo.in>
2021-06-22 14:54:01 +0530

    pipe understanding with observable with of operator

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
+console.clear();
 // Import RxJS;
 import { of, Observable, interval, fromEvent } from 'rxjs';
-import { map, filter, take, takeWhile, delay } from 'rxjs/operators';
+import { map, scan, filter, take, takeWhile, delay } from 'rxjs/operators';
+//------------------------------------------------------------
 interval(1000)
   .pipe(
     takeWhile(() => true),
@@ -8,9 +10,34 @@ interval(1000)
     map(v => Date.now())
   )
   .subscribe(value => console.log('Subscriber: ' + value));
+//-------------------------------------------------------------
+//emits any number of provided values in sequence
+const source = of(1, 2, 3, 4, 5);
+//output: 1,2,3,4,5
+const subscribe = source.subscribe(val => console.log(val));
+//emits values of any type
+const sourceAnyType = of({ name: 'Brian' }, [1, 2, 3], function hello() {
+  return 'Hello';
+});
+//output: {name: 'Brian'}, [1,2,3], function hello() { return 'Hello' }
+const subscribeAnyType = sourceAnyType.subscribe(val => console.log(val));
+const getNumbers = (): Observable<number> => {
+  return of(1, 2, 3, 4, 5, 6, 7);
+};
+const calculateNumbers = (): void => {
+  getNumbers()
+    .pipe(
+      filter(n => n % 2 === 1),
+      map(n => n + 10),
+      scan((sum, n) => sum + n)
+    )
+    .subscribe(result => console.log(result));
+};
+calculateNumbers();
 //emit 1,2,3,4,5
 //const source = of(1, 2, 3, 4, 5);
-
+//-------------------------------------------------------------
 const clicks = fromEvent(document, 'click');
 const delayedClicks = clicks.pipe(delay(1000)); // each click emitted after 1 second
 delayedClicks.subscribe(x => console.log(x));
+//-------------------------------------------------------------
